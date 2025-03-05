6ba986b3e938fba96a0c9ec704c45cf7fa546dbb
Enryki <henriquewatanabe22@gmail.com>
2025-03-05 13:47:33 -0300

    Fix updateStudent function, because I was checking the name instead of email

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -48,7 +48,7 @@ public class StudentService {
 			student.setName(name);
 		}
 
-		if (email != null && email.length() > 0 && !Objects.equals(student.getName(), name)){
+		if (email != null && email.length() > 0 && !Objects.equals(student.getEmail(), email)){
 			Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
 
 			if (studentOptional.isPresent()){
